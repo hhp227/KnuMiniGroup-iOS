05c2f83ee4005afeec48d5bdfd0d413471c2cd0d
hhp227 <hong227@naver.com>
2020-08-23 13:50:39 +0900

    Initialize New Project

---
+++ knu_minigroup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  knu_minigroup
+//
+//  Created by 홍희표 on 2020/08/23.
+//  Copyright © 2020 홍희표. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
