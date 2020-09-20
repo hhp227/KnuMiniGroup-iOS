e4f5089efefdaed6a958dc378180d0ccadff2c18
hhp227 <hong227@naver.com>
2020-09-21 00:13:30 +0900

    2020.09.21 Commit

--- knu_minigroup/Viewcontrollers/MainViewController.swift
+++ knu_minigroup/Viewcontrollers/MainViewController.swift
@@ -8,17 +8,35 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITabBarDelegate {
+    @IBOutlet var mTabBar: UITabBar!
+    @IBOutlet var mFindTabBarItem: UITabBarItem!
+    @IBOutlet var mRequestTabBarItem: UITabBarItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mTabBar.delegate = self
     }
     
     @IBAction func barButtonItemClick(_ sender: UIBarButtonItem) {
-        if let drawerController = parent as? DrawerController {
+        if let drawerController = navigationController?.parent as? DrawerController {
             drawerController.setDrawerState(.opened, animated: true)
         }
     }
     
     
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        switch item {
+        case mFindTabBarItem:
+            let findViewController = storyboard?.instantiateViewController(withIdentifier: "FindViewController") as! FindViewController
+            
+            navigationController?.pushViewController(findViewController, animated: true)
+        case mRequestTabBarItem:
+            let requestViewController = storyboard?.instantiateViewController(withIdentifier: "RequestViewController") as! RequestViewController
+            
+            navigationController?.pushViewController(requestViewController, animated: true)
+        default:
+            break
+        }
+    }
 }
