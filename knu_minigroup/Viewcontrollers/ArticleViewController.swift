3aff8a1b12b8129f0326492e83b1d1dd9be9b851
hhp227 <hong227@naver.com>
2021-03-24 23:26:27 +0900

    2021.03.24 Commit

--- knu_minigroup/Viewcontrollers/ArticleViewController.swift
+++ knu_minigroup/Viewcontrollers/ArticleViewController.swift
@@ -17,6 +17,11 @@ class ArticleViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: view.window)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: view.window)
+    }
+    
     @objc private func keyboardWillShow(_ notification: Notification) {
         if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
             let keyboardRectangle = keyboardFrame.cgRectValue
