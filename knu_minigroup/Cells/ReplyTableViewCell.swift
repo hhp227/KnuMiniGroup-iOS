43daaccb3c380824988601ea0963958e8249ef2d
hhp227 <hong227@naver.com>
2021-06-01 00:59:49 +0900

    2021.06.01 Commit

---
+++ knu_minigroup/Cells/ReplyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReplyTableViewCell.swift
+//  knu_minigroup
+//
+//  Created by 홍희표 on 2021/06/01.
+//  Copyright © 2021 홍희표. All rights reserved.
+//
+
+import UIKit
+
+class ReplyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
