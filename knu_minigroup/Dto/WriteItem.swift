d2eb33088821e453093a8443d5cc3b0592976ae6
hhp227 <hong227@naver.com>
2022-09-27 23:56:30 +0900

    2022.09.27 Commit

---
+++ knu_minigroup/Dto/WriteItem.swift
@@ -0,0 +1,15 @@
+//
+//  WriteItem.swift
+//  knu_minigroup
+//
+//  Created by 홍희표 on 2022/09/27.
+//  Copyright © 2022 홍희표. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum WriteItem {
+    case TextItem(String)
+    case ImageItem(UIImage)
+}
